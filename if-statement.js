02458bc1dfe321fc6e417674ffa655a5f31164a4
rbeeler <ruthbeeler3@gmail.com>
2019-02-13 14:07:20 -0800

    replacing old files with .js files

---
+++ if-statement.js
@@ -0,0 +1,7 @@
+var fruit = 'orange';
+var f = fruit.length;
+if (f > 5){
+  console.log('The fruit name has more than five characters.');
+  }else{
+    console.log('The fruit name has five characters or less.');
+    }
